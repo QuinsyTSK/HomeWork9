13eee198c1efe271aafe3fb301d2ae61cf862d14
полина осиненко <polina_osinenko@mail.ru>
2025-07-20 22:37:44 +0700

    add test for widgit.py, condensed funks modul

--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,10 @@ from src.masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(account_info: str) -> str:
     """Принимаем тип и номер карты, возвращает тип и маску номера"""
+    if account_info == "": # проверяем строку данных на наличие символов
+        raise ValueError("Неверный данные")
+
+
     type_list = []
     number_list = account_info.split()
 
@@ -13,7 +17,10 @@ def mask_account_card(account_info: str) -> str:
     card_type = " ".join(type_list)
 
     # Определяем номер карты
-    card_number = int(number_list[-1])
+    card_number = number_list[-1]
+    for i in card_number: # Проверяем номер на наличие не числовых символов
+        if i.isalpha():
+            raise ValueError("Неверный данные номера")
 
     # Применяем маскировки счета
     if card_type == "Счет":
@@ -24,6 +31,10 @@ def mask_account_card(account_info: str) -> str:
 
 def get_date(date_card: str) -> str:
     """Функция выводит дату в формате ДД.ММ.ГГГГ"""
+
+    if date_card == "": # проверяем строку с данными даты на наличие символов
+        raise ValueError("Неверный ввод даты")
+
     date_list = date_card.split("-", 2)
     date_list[2] = date_list[2][:2]
     date_list_reverse = ".".join(date_list[::-1])
@@ -32,5 +43,6 @@ def get_date(date_card: str) -> str:
 
 if __name__ == "__main__":
     print(mask_account_card("Maestro 1596837868705199"))
+    print(mask_account_card("Visa Classic 6831982476737658"))
     print(mask_account_card("Счет 64686473678894779589"))
     print(get_date("2024-03-11T02:26:18.671407"))
---
+++ tests/test_widget.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.widget import mask_account_card, get_date
+
+
+@pytest.mark.parametrize("account_card , expected",[
+                        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                        ("Счет 64686473678894779589", "Счет **9589"),
+                        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+                        ("Счет 35383033474447895560", "Счет **5560"),
+                        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+                        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+                        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+                        ("Счет 73654108430135874305", "Счет **4305")
+])
+def test_mask_account_card_good(account_card, expected):
+    assert mask_account_card(account_card) == expected
+
+def test_mask_account_card_0():
+    with pytest.raises(ValueError):
+        mask_account_card("")
+
+
+def test_mask_account_card_alpha():
+    with pytest.raises(ValueError):
+        mask_account_card("Visa Gold 599941a228426g53")
+
+
+@pytest.mark.parametrize("date_card, data_list", [("2024-03-11T02:26:18.671407","11.03.2024")])
+def test_get_date_good(date_card, data_list):
+    assert get_date(date_card) == data_list
+
+
+def test_get_date_0():
+    with pytest.raises(ValueError):
+        get_date("")
